df666b73dabb1f12df005b0d9afd3d6727ae226e
Nagalakshmi Selvaraj <lakshmi.padumanabhan@gmail.com>
2017-05-26 19:54:08 +1000

    dabase file is added

---
+++ PayrollSystem/DbScripts_1.sql
@@ -0,0 +1,10 @@
+create table login (Username varchar(255),
+ Password varchar(255),);
+ select *from emp;
+
+ select * from login;
+ 
+ INSERT INTO login(username,password)
+VALUES ('nagu2','nagu');
+select *from login;
+create table employee(firstname varchar(255),lastna
